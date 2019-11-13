ee84f5f5b2af3e33836c603729048ba456da0441
STYZY <styzyreal@126.com>
2019-11-13 12:43:31 +0800

    优化babel实验性特性注释

--- babel.config.js
+++ babel.config.js
@@ -56,7 +56,7 @@ module.exports = function(api) {
 		// Stage 3
 		"@babel/plugin-syntax-dynamic-import",
 		// "@babel/plugin-syntax-import-meta",
-		// ["@babel/plugin-proposal-class-properties", { loose: false }],
+		// "@babel/plugin-proposal-class-properties",
 		// '@babel/plugin-proposal-private-methods',
 		// "@babel/plugin-proposal-json-strings",
     ]
